d9b6f017ee1c7c7e8e902a474f6c128c5f2fda68
darylcitu <daryl.butar@cit.edu>
2025-03-10 21:16:50 +0800

    Text Property

--- Dashboard.Designer.cs
+++ Dashboard.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WildCatTickets
 {
-    partial class Form1
+    partial class Dashboard
     {
         /// <summary>
         /// Required designer variable.
@@ -30,13 +30,13 @@
         {
             this.SuspendLayout();
             // 
-            // Form1
+            // Dashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
-            this.Text = "Splash";
+            this.Name = "Dashboard";
+            this.Text = "Dashboard";
             this.ResumeLayout(false);
 
         }
